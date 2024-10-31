b2492b2d0af86d272fbfc36ffbb4dc9bfd38e771
GRinvest <grfx@live.ru>
2024-11-01 02:49:08 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -954,23 +954,42 @@
     sudo zcat 2016-04-03-yaamp.sql.gz | sudo mysql --defaults-group-suffix=host1
     
     # Oh the humanity!
-    sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2016-04-24-market_history.sql
-    sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2016-04-27-settings.sql
-    sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2016-05-11-coins.sql
-    sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2016-05-15-benchmarks.sql
-    sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2016-05-23-bookmarks.sql
-    sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2016-06-01-notifications.sql
-    sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2016-06-04-bench_chips.sql
-    sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2016-11-23-coins.sql
-    sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2017-02-05-benchmarks.sql
-    sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2017-03-31-earnings_index.sql
-    sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2017-05-accounts_case_swaptime.sql
-    sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2017-06-payouts_coinid_memo.sql
-    sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2017-09-notifications.sql
-    sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2017-10-bookmarks.sql
-    sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2017-11-segwit.sql
-    sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2018-01-stratums_ports.sql
-    sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2018-02-coins_getinfo.sql
+    sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2015-07-01-accounts_hostaddr.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2015-07-12-yaamp.sql.gz
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2015-07-15-coins_hasmasternodes.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2015-09-20-blocks_worker.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2016-02-17-payouts_errmsg.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2016-02-18-accounts_donation.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2016-02-23-shares_diff.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2016-03-26-markets.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2016-03-30-coins.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2016-04-03-accounts.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2016-04-03-yaamp.sql.gz
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2016-04-24-market_history.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2016-04-27-settings.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2016-05-11-coins.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2016-05-15-benchmarks.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2016-05-23-bookmarks.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2016-06-01-notifications.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2016-06-04-bench_chips.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2016-11-23-coins.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2017-02-05-benchmarks.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2017-03-31-earnings_index.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2017-05-accounts_case_swaptime.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2017-06-payouts_coinid_memo.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2017-09-notifications.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2017-10-bookmarks.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2017-11-segwit.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2018-01-stratums_ports.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2018-02-coins_getinfo.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2018-09-22-workers.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2019-03-coins_thepool_life.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2020-06-03-blocks.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2020-11-10-yaamp.sql.gz
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2021-06-21-yaamp.sql.gz
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2022-10-14-shares_solo.sql
+sudo mysql --defaults-group-suffix=host1 --database=yiimpfrontend --force < 2022-10-29-blocks_effort.sql
+
     echo -e "$GREEN Done...$COL_RESET"
         
     
